310ab007796176d22d23284ebd12567cee297668
laag-dev <laag.08@gmail.com>
2024-03-09 00:00:21 -0500

    actualización en Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,66 @@
-import './App.css';
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 import Contacto from "./components/Contacto";
-import { Service } from './components/Service';
-
+import { Service } from "./components/Service";
+import { AuthProvider } from "./context/AuthContext";
+import UserLogin from "./components/UserLogin";
+import UserRegistration from "./components/UserRegistration";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isRegistrationModalOpen, setIsRegistrationModalOpen] = useState(false);
+
+  const openLoginModal = () => {
+    console.log("Abriendo modal de inicio de sesión");
+    setIsLoginModalOpen(true);
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
+
+  const openRegistrationModal = () => {
+    console.log("Abriendo modal de registro de usuario");
+    setIsRegistrationModalOpen(true);
+  };
+
+  const closeRegistrationModal = () => {
+    setIsRegistrationModalOpen(false);
+  };
 
   return (
-    <div className="App">    
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/contacto' element={<Contacto />} />
-        <Route path='/service' element={<Service/>} />
-      </Routes> 
+    <AuthProvider>
+      <div className="App">
+        <NavBar
+          openLoginModal={openLoginModal}
+          openRegistrationModal={openRegistrationModal}
+        />
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <UserLogin
+                isOpenL={isLoginModalOpen}
+                onCloseL={closeLoginModal}
+              />
+            }
+          />
+          <Route
+            path="/registration"
+            element={
+              <UserRegistration
+                isOpen={isRegistrationModalOpen}
+                onClose={closeRegistrationModal}
+              />
+            }
+          />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/service" element={<Service />} />
+        </Routes>
       </div>
-  )
+    </AuthProvider>
+  );
 }
-
-export default App
